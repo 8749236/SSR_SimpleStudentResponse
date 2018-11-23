5dc7434941613c999424366b2ba6fac2ccbb21e7
Dennis Liu <dennis.liu2013@gmail.com>
2018-11-22 23:05:02 -0500

    - Added issue prompt to question editor using HTML5 validation and  styled with Bootstrap 4  - Added validate button to question editor  - Updated TODO list in homepage

--- frontend/js/controllers/QuestionEditorController.js
+++ frontend/js/controllers/QuestionEditorController.js
@@ -1,12 +1,23 @@
 app.controller('QuestionEditorController', function($scope, $location) {
 		$scope.question = new MultipleChoiceQuestion();
+		$scope.issues = [];
+		var editorForm = document.getElementById("questionSubmissionForm");
+		
+		$scope.onReset = function() {
+			$scope.issues = [];
+			editorForm.classList.remove('was-validated');
+		};
+		
+		$scope.onValidate = function() {
+			editorForm.classList.add('was-validated');
+			$scope.issues = $scope.question.validate();
+			return $scope.issues;
+		};
 	
 		$scope.onComposeQuestion = function() {
-			var issues = $scope.question.validate();
-			if(Object.keys(issues).length > 0) {
-				$scope.issues = issues;
-			} else {
-				$scope.question.upload();
+			var issues = $scope.onValidate();
+			if(Object.keys(issues).length == 0) {
+				//$scope.question.upload();
 			}
-		}
+		};
 });
