4a8d0bdf6310a44fb7be2d15b4266935f6942843
Dennis Liu <dennis.liu2013@gmail.com>
2018-12-08 23:25:50 -0500

    - Completely reworked question statistic page    - Question preview and sharing is now achieve via collapsible cards      which are hidden by default, making entire page much cleaner    - Add control buttons to provide more controls to users    - Improved responsiveness across different devices  - Actually implemented statistic.js, however not yet complete  - Modified StatisticController.js to support reworked statsitic page    also included dummy data for "Analyze Data" button, still WIP

--- frontend/js/controllers/StatisticController.js
+++ frontend/js/controllers/StatisticController.js
@@ -1,16 +1,53 @@
-app.controller("StatisticController", function($scope, $http, $location, $routeParams, SSEService) {
+app.controller("StatisticController", function($scope, $http, $location, $routeParams, SSEService, ModalDataService) {
 	var questionStore = new QuestionStore();
 	var responseStore = new ResponseStore();
+	var statisticStore = new StatisticStore();
 	var chart = null;
 	var absLinkPrefix = "/questions/"
 	$scope.statistic = {};
 	$scope.question = {};
 	$scope.questionId = $routeParams.question_id;
+	$scope.live = true;
 	
 	SSEService.start($scope);
 	
+	//$scope.onPreviewQuestion = function() {
+	//	ModalDataService.setData("modalQuestionDisplay", {question: $scope.question});
+	//}
+	
+	$scope.onToggleMonitoring = function() {
+		$scope.live = !$scope.live;
+		$scope.statusText = $scope.live ? "Live" : "Paused";
+		console.log("Monitoring Status:", $scope.live);
+	}
+	
+	$scope.onAnalyzeData = function() {
+		//var data = $scope.statistic.getProcessedData();
+		var data = [
+			{
+			name: "Total selection count",
+			data: [2, 4, 7, 3]
+			},
+			{
+			name: "Attempts 1",
+			data: [4, 2, 3, 7]
+			},
+			{
+			name: "Attempts 2",
+			data: [0, 5, 5, 6]
+			},
+		];
+		for(var i in data) {
+			if(chart.series[i]) {
+				chart.series[i].update(data[i]);
+			} else {
+				chart.addSeries(data[i]);
+			}
+		}
+	}
+	
 	$scope.onQuestionGetSuccess = function(question) {
-		console.log("Acquired question: ", question);
+		//console.log("Acquired question: ", question);
 		$scope.question = question;
 		$scope.link = window.location.origin + absLinkPrefix + question.id;
 		$scope.statistic.question = question;
@@ -21,10 +58,14 @@ app.controller("StatisticController", function($scope, $http, $location, $routeP
 		}
 		chart.xAxis[0].setExtremes(0, $scope.statistic.choiceCount - 1);
 		chart.xAxis[0].setCategories(category, true);
+		
+		// Ask for responses
+		responseStore.getResponseByQuestionId($scope.questionId, $scope.onResponsesGetSuccess);
+		$scope.$apply();
 	};
 	
-	$scope.onResponsesGetSuccess = function(statistics) {
-		console.log("Acquired statistic: ", statistics);
+	$scope.onResponsesGetSuccess = function(responses) {
+		//console.log("Acquired responses: ", responses);
 		
 		$scope.statistic.data = [];
 		for(var i = 1; i <= $scope.statistic.choiceCount; ++i) {
@@ -33,14 +74,19 @@ app.controller("StatisticController", function($scope, $http, $location, $routeP
 		// Remove duplicated response
 		// Keep move recent responses
 		// Compute counts per choice
-		for(var k in statistics) {
-			var resp = statistics[k];
+		for(var k in responses) {
+			var resp = responses[k];
 			if(Number.isInteger($scope.statistic.data[resp.answer])) {
 				$scope.statistic.data[resp.answer] += 1;
 			}
 		}
 		// Update (initialize) highcharts
 		chart.series[0].setData($scope.statistic.data.slice());
+		
+		// Build a statistic object
+		responses.type = $scope.question.type;
+		var tmp = statisticStore.buildStatistic(responses);
+		console.log(tmp);
 	}
 	
 	$scope.$on('$viewContentLoaded', function() {
@@ -51,9 +97,15 @@ app.controller("StatisticController", function($scope, $http, $location, $routeP
 	//});
 	
 	$scope.$on("SSEService.questions." + $scope.questionId + ".responses.update", function(e, data) {
-		console.log("Incoming update for responses", e, data);
+		//console.log("Incoming update for responses", e, data);
 		$scope.statistic.data[data.answer] += 1;
-		chart.series[0].setData($scope.statistic.data);
+		// Add new data to the statistic object
+		//$scope.statistic.addData(data);
+		
+		// If still showing live statistic, update chart
+		if($scope.live) {
+			chart.series[0].setData($scope.statistic.data);
+		}
 	});
 		
 	// Initialization
@@ -64,9 +116,6 @@ app.controller("StatisticController", function($scope, $http, $location, $routeP
 		},
 		chart: {
 			type: 'column',
-		},
-		legend: {
-			enabled: false
 		},
 		xAxis: {
 			title: {
@@ -85,9 +134,6 @@ app.controller("StatisticController", function($scope, $http, $location, $routeP
 	
 	// Ask for questions
 	questionStore.getQuestion($scope.questionId, $scope.onQuestionGetSuccess);
-
-	// Ask for statistics
-	responseStore.getResponseByQuestionId($scope.questionId, $scope.onResponsesGetSuccess);
 
 	// the button action
 	$('#button').click(function () {
--- frontend/js/models/statistic.js
+++ frontend/js/models/statistic.js
@@ -7,15 +7,15 @@ class StatisticStore {
 	constructor() {}
 	
 	buildStatistic(data) {
-		var questionType = QuestionStore.typeMap[data.type];
-		var question = new questionType(data);
+		var statisticType = StatisticStore.typeMap[data.type];
+		var statistic = new statisticType(data);
 		console.log(data);
-		return question;
+		return statistic;
 	}
 	
 	_defaultGetSuccess() {
 		console.log(data);
-		alert("Successfully retrieved a question");
+		alert("Successfully retrieved a statistic");
 	}
 
 }; StatisticStore.typeMap = {};
@@ -37,28 +37,85 @@ class StatisticStore {
 class StatisticBase extends ICRUD {
 	constructor(rawData) {
 		super();
-		rawData = rawData ? rawData : {};
-		this.id = rawData.title ? rawData.title : null;
-		this.type = rawData.type ? rawData.type : null;
-		this.data = rawData.data ? rawData.data : null;
+		this.type = rawData.type;
+		this.rawData = rawData ? rawData : {};
+		this.processors = {};
 	}
+	
+	getProcessedData() {
+		var rslt = {};
+		for(var key in this.processors) {
+			rslt[key] = this.processors[key](this.rawData);
+		}
+		return rslt;
+	}
+	
+	addProcessor(key, processorFunc) {
+		this.processors[key] = processorFunc;
+	}
+	
+	removeProcessor(key) {
+		delete this.processors[key];
+	}
+	
 }
 
 // Abstract class for statistic of individual questions
 class QuestionStatisticBase extends StatisticBase {
+	constructor(rawData) {
+		super(rawData)
+		this.respByAttempts = this._partitionDataByAttempts(this.rawData);
+	}
 	
+	_partitionDataByAttempts(rawData) {
+		console.log(rawData);
+		var rslt = null;
+		var respByUser = rawData.reduce((acc, resp) => (acc[resp.owner] = [], acc), {});
+		var highestAttempt = 0;
+		// store responses by username
+		rawData.reduce((acc, resp) => (acc[resp.owner].push(resp), acc), respByUser);
+		// Get highest attempt and prepare final result holder
+		for(var username in respByUser) {
+			if(respByUser[username].length > highestAttempt) {
+				highestAttempt = respByUser[username].length;
+			}
+		}
+		// new Array with preset length has to be filled for ".forEach" to work
+		rslt = new Array(highestAttempt).fill(null);
+		rslt.forEach((v, i, arr) => arr[i] = new Array());
+		
+		// Sort responses under each user in ascending order base on time created
+		// Ideally problem size is greatly reduced (divided by number of user)
+		// it should be cheap to sort
+		for(var username in respByUser) {
+			respByUser[username].sort((r1, r2) => (r1 == r2 ? 0 : (r1>r2?1:-1)));
+			// With an array of sorted responses, index will be their attempt number
+			for(var attempt in respByUser[username]) {
+				rslt[attempt].push(respByUser[username][attempt]);
+			}
+		}
+		return rslt;
+	}
 }
 
 
-class MultipleAnswerStatistic extends QuestionBase {
+class MultipleAnswerStatistic extends QuestionStatisticBase {
 	constructor(rawData) {
 		super(rawData);
+		this.addProcessor("respCountByChoice");
 	}
-}; StatisticStore.typeMap.mult_answer = MultipleAnswerQuestion;
+	
+	_respCountByChoice(rawData) {
+		var accumulator = rawData.reduce((acc, resp)=>(acc[resp.answer]=0, acc), []);
+		var respReducer = (acc, resp) => (acc[resp.answer]+= 1, acc);
+		return rawData.reduce(respReducer, accumulator);
+	}
+	
+}; StatisticStore.typeMap.mult_answer = MultipleAnswerStatistic;
 
-class MultipleChoiceStatistic extends MultipleAnswerQuestion {
+class MultipleChoiceStatistic extends MultipleAnswerStatistic {
 	constructor(rawData) {
 		rawData = rawData ? rawData : {};
 		super(rawData);
 	}
-}; StatisticStore.typeMap.mult_choice = MultipleChoiceQuestion;
+}; StatisticStore.typeMap.mult_choice = MultipleChoiceStatistic;
